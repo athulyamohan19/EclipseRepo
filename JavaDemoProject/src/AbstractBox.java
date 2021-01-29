9a0de0fd29e0b4a85773a101ab60ac270ceea961
athulyamohan19 <athulyamohan19@gmail.com>
2021-01-29 17:41:51 +0530

    TestClass added

--- JavaDemoProject/src/AbstractBox.java
+++ JavaDemoProject/src/AbstractBox.java
@@ -1,13 +1,7 @@
-package javaBasics;
 
-public class AbstractBox extends AbstractContainer {
 
-	@Override
-	int AreaOf(int height) 
-	{
-		int total2=height+100;
-		System.out.println("Box value : "+total2);
-		return total2;
-	}
+public class AbstractBox  {
+
+	
 
 }
