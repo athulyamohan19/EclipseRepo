14e355a3cff321bbb40882a0fa2e52cd805f8d2b
athulyamohan19 <athulyamohan19@gmail.com>
2021-01-29 16:43:19 +0530

    Initial Commit

---
+++ JavaDemoProject/src/amstrong.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class amstrong 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int n=sc.nextInt();
+		int t=n;
+		int s=0;
+		while(t!=0)
+		{
+			int r=t%10;
+			s=s+(r*r*r);
+			t=t/10;
+		}
+		if(s==n)
+			System.out.println("Amstrong");
+		else
+			System.out.println("Not Amstrong");
+		
+		
+	}
+		 
+}
---
+++ JavaDemoProject/src/wrapperclass.java
@@ -0,0 +1,9 @@
+
+
+public class wrapperclass {
+	public static void main(String[] args) {
+		String age="29";
+		int a=Integer.parseInt(age)+1;
+		System.out.println(a);
+	}
+}
